12c5e6862363aa67dc604aa1c2ebab1d1c4d0686
ItzelQuintana <ovalli0326@gmail.com>
2024-01-09 22:44:39 -0600

    Se realiza la conexión con la base de datos

--- connect.js
+++ connect.js
@@ -1,10 +1,18 @@
-const config = require('./config');
 
+const { MongoClient } = require('mongodb');
+const config = require('./config');
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
+const client = new MongoClient(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connect() {
-  // TODO: Database Connection
+  try {
+    await client.connect();
+    const db = client.db('burgerqueen06');
+    console.log('Base de datos conectada');
+    return db;
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error.message);
+  }
 }
-
 module.exports = { connect };
